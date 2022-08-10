96d9bd92f235080712b7e6d673099eb90c2cb8a0
aryanrana99 <aryan.rana21032002@gmail.com>
2022-08-11 01:35:22 +0530

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+fetch('https://jsonplaceholder.typicode.com/posts?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09')
+.then(res => res.json())
+.then(data => console.log(data))
+
+
+
