e49cca3a173a309e9ffe26d19ed44661c6d18a02
aryanrana99 <aryan.rana21032002@gmail.com>
2022-08-11 14:24:26 +0530

    init

--- script.js
+++ script.js
@@ -2,5 +2,30 @@ fetch('https://jsonplaceholder.typicode.com/posts?utm_source=Mailerlite&utm_medi
 .then(res => res.json())
 .then(data => console.log(data))
 
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
 
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+  searchIcon.classList.remove('fa-times');
+  searchForm.classList.remove('active');
+}
+
+let searchIcon = document.querySelector('#search-icon');
+let searchForm = document.querySelector('.search-form');
+
+searchIcon.onclick = () =>{
+  searchIcon.classList.toggle('fa-times');
+  searchForm.classList.toggle('active');
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  searchIcon.classList.remove('fa-times');
+  searchForm.classList.remove('active');
+}
 
